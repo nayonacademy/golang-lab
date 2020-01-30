9047e1a5d3002c3274f428c18f571efe8643a39f
nayonacademy <si.nayon@gmail.com>
2020-01-30 17:16:32 +0600

    coursera

--- struct/default.go
+++ struct/default.go
@@ -1,1 +1,38 @@
 package main
+type Gender string
+
+type Sample struct {
+	Name   string `default:"John Smith"`
+	Age    int    `default:"27"`
+	Gender Gender `default:"m"`
+
+	Slice       []string       `default:"[]"`
+	SliceByJSON []int          `default:"[1, 2, 3]"` // Supports JSON
+	Map         map[string]int `default:"{}"`
+	MapByJSON   map[string]int `default:"{\"foo\": 123}"`
+
+	Struct    OtherStruct  `default:"{}"`
+	StructPtr *OtherStruct `default:"{\"Foo\": 123}"`
+
+	NoTag  OtherStruct               // Recurses into a nested struct by default
+	OptOut OtherStruct `default:"-"` // Opt-out
+}
+
+type OtherStruct struct {
+	Hello  string `default:"world"` // Tags in a nested struct also work
+	Foo    int    `default:"-"`
+	Random int    `default:"-"`
+}
+
+// SetDefaults implements defaults.Setter interface
+func (s *OtherStruct) SetDefaults() {
+	if defaults.CanUpdate(s.Random) { // Check if it's a zero value (recommended)
+	s.Random = rand.Int() // Set a dynamic value
+	}
+}
+func main(){
+	obj := &Sample{}
+	if err := defaults.Set(obj); err != nil {
+		panic(err)
+	}
+}
--- struct/meetup.go
+++ struct/meetup.go
@@ -1,1 +1,50 @@
 package main
+
+import "fmt"
+type PersonNew interface {
+	SayHello()
+	GetDetails()
+}
+type PersonNew struct {
+	name string
+	age int
+	city, phone string
+}
+
+func (p PersonNew) SayHello(){
+	fmt.Println("Hello %s", p.name)
+}
+
+func (p PersonNew) GetDetails(){
+	fmt.Println("Hello Mr. %s you live in %s and your phone no is %s , now you are %d age old",p.name, p.city, p.phone,p.age)
+}
+type Speaker struct {
+	person PersonNew
+	speaksOn []string
+	pastEvents []string
+}
+func (s Speaker) GetDetails(){
+	s.person.GetDetails()
+	fmt.Println("Speaker talks on following technologies:")
+	for _, value := range s.speaksOn {
+		fmt.Println(value)
+	}
+	fmt.Println("Presented on the following conferences:")
+	for _, value := range s.pastEvents {
+		fmt.Println(value)
+	}
+}
+type Organization struct {
+	PersonNew
+	meetups []string
+}
+func (o Organization) GetDetails(){
+	o.PersonNew.GetDetails()
+	for _, value := range o.meetups{
+		fmt.Println(value)
+	}
+}
+func main(){
+	//p := PersonNew{name:"newton",age:60, city:"London", phone:"29864982374"}
+	//fmt.Println(p)
+}
